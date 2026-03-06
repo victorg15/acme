8a3b1c7885c8bc47d2614eb6e0dbed6d32ea0fd8
Victor Gabriel <victorgabrieel8@gmail.com>
2026-03-06 22:46:52 +0000

    chore: implementar moldura raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+import {inter} from '@/app/ui/fonts';
+import '@/app/globals.css';
+
+export const metadata: Metadata = {
+  title: 'ACME'
+};
+
+export default function RootLayout({children}: {children:React.ReactNode}) {
+  return (
+    <html lang ='pt-br'>
+      <body className={inter.className}>
+        { children }
+      </body>
+    </html>
+  );
+}
